093216812d07c21ef10771bc1a2a5c765e1b091d
16272926 <16272926@uepg.br>
2019-06-03 13:39:31 -0300

    inserção bootstrap e layout

---
+++ resources/views/teste.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link href="css/teste.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-grid.css" rel="stylesheet">
+    <link href="css/bootstrap-grid.min.css" rel="stylesheet">
+    <link href="css/bootstrap-reboot.css" rel="stylesheet">
+    <link href="css/bootstrap-reboot.min.css" rel="stylesheet">
+</head>
+<body>
+    <fieldset id="cores">
+            <button type="button" id="botao" class="btn btn-primary"></button><br><br>
+            <button type="button" id="botao" class="btn btn-success"></button><br><br>
+            <button type="button" id="botao" class="btn btn-danger"></button><br><br>
+            <button type="button" id="botao" class="btn btn-warning"></button><br><br>
+    </fieldset>
+</body>
+</html>
+
+
+
