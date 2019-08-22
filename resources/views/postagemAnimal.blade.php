6573aacce6d8e43025addf0826fe49dbc055e310
PedroBrandalise <pedroaugustopb@gmail.com>
2019-08-22 10:36:53 -0300

    melhorar bootstrap na pagina de postagens

--- resources/views/postagemAnimal.blade.php
+++ resources/views/postagemAnimal.blade.php
@@ -44,51 +44,53 @@
 
 
 
+<div class="card">
+<div class="card-body">
 
-<div class="row " id="campo">
-    <div class="col-6 " style="align:center;">
-        <img src="{{url('img/dog.jpeg')}}" class="img-fluid rounded" >
-
-    </div>
-    
-    <div class="col-6 ">
-        <br>
-        <b>Nome:</b>  {{$postagem->nome_animal}} <br>
-        <b>Sexo:</b>  {{$postagem->sexo}} <br>
-
-        <b>Idade:</b> 
-        @php
+    <div class="row ">
+        <div class="col-6 " style="align:center;">
+                <img src="{{url('img/dog.jpeg')}}" class="img-fluid rounded" >
+                
+            </div>
+            
+            <div class="col-6 ">
+                <br>
+                <b>Nome:</b>  {{$postagem->nome_animal}} <br>
+                <b>Sexo:</b>  {{$postagem->sexo}} <br>
+                
+                <b>Idade:</b> 
+                @php
         
-            if ($postagem->nascimento!=NULL) {
-                // dd($postagem->nascimento);
-                $idade=ListaController::calcIdade($postagem->nascimento);
-                if ($idade<1.0) {
-                    echo 'Menos de um ano<br>';
-                }elseif ($idade==1.0) {
-                    echo '1 ano<br>';
+        if ($postagem->nascimento!=NULL) {
+            // dd($postagem->nascimento);
+            $idade=ListaController::calcIdade($postagem->nascimento);
+            if ($idade<1.0) {
+                echo 'Menos de um ano<br>';
+            }elseif ($idade==1.0) {
+                echo '1 ano<br>';
                 }else{
                     echo (int) $idade." anos<br>";
                 }
             }
-        @endphp
+            @endphp
         <b>Porte:</b> {{$postagem->tipo_porte}} <br>
         <b>Dono da postagem: </b> <a href="{{url('/perfil'.$postagem->cod_usuario_postagem)}}">{{$postagem->nome}}</a><br>
         <b>Avaliação do dono da postagem:</b> 
         
         @if (PerfilController::getAvaliacao($postagem->cod_usuario_postagem) !=NULL)
-            @for ($i = 0; $i < (int)PerfilController::getAvaliacao($postagem->cod_usuario_postagem); $i++)
-                <i class="material-icons">pets</i>
-            @endfor
+        @for ($i = 0; $i < (int)PerfilController::getAvaliacao($postagem->cod_usuario_postagem); $i++)
+        <i class="material-icons">pets</i>
+        @endfor
         @else
-           Usuario ainda não avaliado
+        Usuario ainda não avaliado
         @endif
         
-
+        
         {{-- <i class="material-icons">pets</i>
         <i class="material-icons">pets</i> 
         <i class="material-icons">pets</i>
         <i class="material-icons">pets</i>
-         --}}
+        --}}
         
         <br>
         <a href="{{url('postagem/'.$postagem->cod_postagem.'/solicitar')}}"> 
@@ -97,35 +99,57 @@
         <a href="{{url('postagem/'.$postagem->cod_postagem.'/denunciar')}}">
             <button class =" btn btn-danger"> Denunciar</button>    
         </a>
-            
+        
     </div>
-
+    
+</div>
+</div>
 </div>
 
-<div class="row" id="campo">
+<br>
+
+<div class="card">
+    <div class="card-body">
+
+            <div class="row ">
     <div class="container-fluid">
         <h2>Descrição</h2>
         <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem explicabo quidem consequatur, laudantium aut architecto sit voluptatum earum quas facilis eum iste eius ab. Vel tempora minus error nostrum dolorum. 
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum at suscipit neque magnam, nesciunt quia temporibus expedita earum molestiae vero aliquid quidem reprehenderit! Impedit cum repellendus veniam nesciunt obcaecati. Accusamus.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem alias totam vitae est quibusdam nostrum, sint perferendis ullam itaque, voluptate voluptatem molestiae! Nostrum, excepturi culpa quos fuga eligendi corrupti eum.
+            {{$postagem->descricao}}
         
         </p>
     </div>
 </div>
-<div class="row" id="campo">
-    <div class="container-fluid">
-        <h2>Descrição Saude</h2>
+</div>
+</div>
+
+<br>
+
+<div class="card">
+        <div class="card-body">
+
+                <div class="row ">
+        <div class="container-fluid">
+        <h2>Saúde</h2>
+        
         <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem explicabo quidem consequatur, laudantium aut architecto sit voluptatum earum quas facilis eum iste eius ab. Vel tempora minus error nostrum dolorum. 
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum at suscipit neque magnam, nesciunt quia temporibus expedita earum molestiae vero aliquid quidem reprehenderit! Impedit cum repellendus veniam nesciunt obcaecati. Accusamus. 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem alias totam vitae est quibusdam nostrum, sint perferendis ullam itaque, voluptate voluptatem molestiae! Nostrum, excepturi culpa quos fuga eligendi corrupti eum.
+            Castrado: {{str_replace("a", "ã", $postagem->castrado)}}
         </p>
-    
+        <p>
+            Vacinado: {{str_replace("a", "ã", $postagem->vacinacao_em_dia)}}
+        </p>
+        <p>
+            Vermifugado: {{str_replace("a", "ã", $postagem->vermifugado)}}
+        </p>
+        <h3>Descrição</h3>
+        <p>
+            {{$postagem->descricao_saude}}
+        </p>
     </div>
 
 </div>
-
+</div>
+</div>
 
 
 @endsection
