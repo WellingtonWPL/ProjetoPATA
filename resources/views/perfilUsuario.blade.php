32381712fb65ddbeb1dae32e0a9c28668d0fa862
PedroBrandalise <pedroaugustopb@gmail.com>
2019-08-27 16:12:34 -0300

    alterações no perfil

--- resources/views/perfilUsuario.blade.php
+++ resources/views/perfilUsuario.blade.php
@@ -1,96 +1,133 @@
 @extends('template')
 @section('css')
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <style>
-        i {
-            font-size: 24px !important;
-            vertical-align:middle;
-        }
-        div a img{
+<link rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<style>
+    i {
+        font-size: 24px !important;
+        vertical-align: middle;
+    }
+
+    div a img {
         margin: 2%;
         border-style: solid;
         border-width: 1px;
         border-color: #F5F5F5;
         /* width: 100%; */
         max-height: 150px;
-        padding:10px;
-        
+        padding: 10px;
+
 
     }
 
     #campo {
         border-style: solid;
-        border-width: 1px; 
+        border-width: 1px;
         border-color: #F5F5F5;
-        border-radius:15px;
+        border-radius: 15px;
         margin-top: 3%;
         /* max-height: 160px; */
         background-color: #FAFAFA;
     }
 
     #perfil {
-        border-width: 1px; 
-        border-radius:15px;
+        border-width: 1px;
+        border-radius: 15px;
         /* margin-top: 15%; */
-        
+
     }
 
 
 
-    div button{       
+    div button {
         margin: 2%;
     }
 
-    div img{
+    div img {
         margin: 2%;
         border-style: solid;
         border-width: 1px;
         /* width: 100%; */
         max-height: 250px;
-        padding:10px;
-        
+        padding: 10px;
+
 
     }
-
-    </style>
+</style>
 @endsection
 
 @section('conteudo')
-<div id="perfil"  style="float: right; align:center;">
-    <img src="{{url('img/perfil.jpg')}}" class="img-fluid rounded" >
+
+@php
+use App\Http\Controllers\PerfilController;
+//  dd($usuario);
+$donoPerfil = False;
+
+if (Auth::check()){
+    $usuarioLogado =(Auth::user());
+    if($usuarioLogado->cod_usuario==$cod_usuario){
+        $donoPerfil = True;
+    }
+}
+
+$avaliacao = PerfilController::getAvaliacao($cod_usuario);
+
+
+@endphp
+
+
+<div id="perfil" style="float: right; align:center;">
+    <img src="{{url('img/perfil.jpg')}}"
+        class="img-fluid rounded">
     <br>
     <div align="center" style="align:center;">
-        <b>Avaliação:</b> 
-            <i class="material-icons">pets</i>
-            <i class="material-icons">pets</i>
-            <i class="material-icons">pets</i>
-            <i class="material-icons">pets</i>
-            <i class="material-icons">pets</i>
+        <b>Avaliação:</b>
+        @php
+            if ($avaliacao==0) {
+                echo 'Sem avaliações';
+            }
+             for($i =0; $i<$avaliacao; $i++){
+
+                 echo '<i class="material-icons">pets</i>';
+             }
+        @endphp
+
     </div>
-    
+
 </div>
 <div class="row" id="campo">
-    <div style="margin-left: 5%" >
+    <div style="margin-left: 5%">
         <br><br>
-        <b>Nome:</b><h2>Pedro Brandalise</h2><br>
-        <b>Cidade:</b><h4>Ponta Grossa - PR</h4><br>
-    </div> 
-    <div style="margin-left: 2%" class="container-fluid"> 
-        <button class=" btn btn-primary"><i class="material-icons" style>announcement</i> Notificações</button> 
-        <button class=" btn btn-alert"><i class="material-icons" style>edit</i> Editar</button> 
-        <button class=" btn btn-success"><i class="material-icons" style>library_add</i> Novo post</button>
-        <button class=" btn btn-danger"><i class="material-icons" style>delete</i> Deletar</button>            
+        <b>Nome:</b>
+        <h2>{{  $usuario[0]->nome  }}</h2><br>
+        <b>Cidade:</b>
+        <h4>Ponta Grossa - PR</h4><br>
     </div>
+    @if ($donoPerfil==True)
+
+    <div style="margin-left: 2%" class="container-fluid">
+        <button class=" btn btn-primary"><i
+                class="material-icons"
+                style>announcement</i> Notificações</button>
+        <button class=" btn btn-alert"><i
+                class="material-icons" style>edit</i>
+            Editar</button>
+        <button class=" btn btn-success"><i
+                class="material-icons" style>library_add</i>
+            Novo post</button>
+        <button class=" btn btn-danger"><i
+                class="material-icons" style>delete</i>
+            Deletar</button>
+    </div>
+    @endif
 </div>
 <div class="row" id="campo">
     <div class="container-fluid">
         <br>
         <h2>Descrição</h2>
         <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem explicabo quidem consequatur, laudantium aut architecto sit voluptatum earum quas facilis eum iste eius ab. Vel tempora minus error nostrum dolorum. 
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum at suscipit neque magnam, nesciunt quia temporibus expedita earum molestiae vero aliquid quidem reprehenderit! Impedit cum repellendus veniam nesciunt obcaecati. Accusamus.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem alias totam vitae est quibusdam nostrum, sint perferendis ullam itaque, voluptate voluptatem molestiae! Nostrum, excepturi culpa quos fuga eligendi corrupti eum.
-        
+            {{  $usuario[0]->descricao  }}
+
         </p>
     </div>
 </div>
@@ -99,49 +136,58 @@
 </div>
 <div class="row " id="campo">
 
-        <div class="col-3">
-            <a href="{{url('/postagem/1')}}">
-                    <img src="{{url('img/dog.jpeg')}}" class="img-fluid rounded" >
-                </a>
-        </div>
-        <div class="col-9">
-                <a href="{{url('/postagem/1')}}"><h2>Totó</h2> </a>
-                Macho <br>
-                4 meses <br>
-                Porte médio <br>
-        </div>
-
+    <div class="col-3">
+        <a href="{{url('/postagem/1')}}">
+            <img src="{{url('img/dog.jpeg')}}"
+                class="img-fluid rounded">
+        </a>
     </div>
-        
-
-    <div class="row " id="campo">
-        <div class="col-3">
-            <a href="{{url('/postagem/1')}}">
-                    <img src="{{url('img/dog2.jpeg')}}" class="img-fluid rounded" >
-                </a>
-        </div>
-        <div class="col-9">
-                <a href="{{url('/postagem/1')}}"><h2>Rex</h2> </a>
-                Macho <br>
-                2 anos <br>
-                Porte médio <br>
-        </div>
-
+    <div class="col-9">
+        <a href="{{url('/postagem/1')}}">
+            <h2>Totó</h2>
+        </a>
+        Macho <br>
+        4 meses <br>
+        Porte médio <br>
     </div>
 
+</div>
 
-    <div class="row " id="campo">
-        <div class="col-3">
-            <a href="{{url('/postagem/1')}}">
-                    <img src="{{url('img/gato.jpeg')}}" class="img-fluid rounded" >
-                </a>
-        </div>
-        <div class="col-9">
-                <a href="{{url('/postagem/1')}}"><h2>Miau</h2> </a>
-                Macho <br>
-                
-                Porte pequeno <br>
-        </div>
 
+<div class="row " id="campo">
+    <div class="col-3">
+        <a href="{{url('/postagem/1')}}">
+            <img src="{{url('img/dog2.jpeg')}}"
+                class="img-fluid rounded">
+        </a>
     </div>
+    <div class="col-9">
+        <a href="{{url('/postagem/1')}}">
+            <h2>Rex</h2>
+        </a>
+        Macho <br>
+        2 anos <br>
+        Porte médio <br>
+    </div>
+
+</div>
+
+
+<div class="row " id="campo">
+    <div class="col-3">
+        <a href="{{url('/postagem/1')}}">
+            <img src="{{url('img/gato.jpeg')}}"
+                class="img-fluid rounded">
+        </a>
+    </div>
+    <div class="col-9">
+        <a href="{{url('/postagem/1')}}">
+            <h2>Miau</h2>
+        </a>
+        Macho <br>
+
+        Porte pequeno <br>
+    </div>
+
+</div>
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,40 +4,60 @@
 <head>
 
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
 
   <title>Projeto - PATA</title>
-  
+
   <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" type="text/css" href="/css/teste.css">
-  <link rel="stylesheet" type="text/css" href="/css/bootstrap.css" >
-  <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="/css/bootstrap-grid.css">
-  <link rel="stylesheet" type="text/css" href="/css/bootstrap-grid.min.css">
-  <link rel="stylesheet" type="text/css" href="/css/bootstrap-reboot.css">
-  <link rel="stylesheet" type="text/css" href="/css/bootstrap-reboot.min.css">
-  
+  <link rel="stylesheet" type="text/css"
+    href="/css/teste.css">
+  <link rel="stylesheet" type="text/css"
+    href="/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css"
+    href="/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css"
+    href="/css/bootstrap-grid.css">
+  <link rel="stylesheet" type="text/css"
+    href="/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" type="text/css"
+    href="/css/bootstrap-reboot.css">
+  <link rel="stylesheet" type="text/css"
+    href="/css/bootstrap-reboot.min.css">
+
   <!-- Custom fonts for this template -->
-  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
-  
+  <link
+    href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900"
+    rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i"
+    rel="stylesheet">
+
   <!-- Custom styles for this template -->
   <link href="/css/one-page-wonder.css" rel="stylesheet">
-  
-  <!-- Function JS -->
-  <script type="text/javascript" src="/js/functions.js"></script>
-  @yield('css')
-  
-  {{-- <script src="/vendor/jquery/jquery.min.js"></script> --}}
-  <script
-  src="https://code.jquery.com/jquery-3.4.1.js"
-  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-  
-  crossorigin="anonymous"></script>
 
-</head> 
+  <!-- Function JS -->
+  <script type="text/javascript" src="/js/functions.js">
+  </script>
+  @yield('css')
+
+  {{-- <script src="/vendor/jquery/jquery.min.js"></script> --}}
+  <script src="https://code.jquery.com/jquery-3.4.1.js"
+    integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+    crossorigin="anonymous"></script>
+
+  <style>
+    ul li {
+      list-style: none;
+    }
+
+    ul li a {
+      color: aliceblue;
+    }
+  </style>
+</head>
 
 <body>
   <script>
@@ -46,80 +66,119 @@
     });
   </script>
   <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+  <nav
+    class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
     <div class="container">
-    <a class="navbar-brand" href="{{url('/lista')}}">Pata</a>
-      <button class="navbar-toggler"  data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="navbar-brand"
+        href="{{url('/lista')}}">Pata</a>
+      <button class="navbar-toggler" data-toggle="collapse"
+        data-target="#navbarResponsive"
+        aria-controls="navbarResponsive"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-              <a class="nav-link" href="{{url('/lista')}}">Lista</a>
-          
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="{{url('perfil')}}">Perfil</a>
-          </li>
+        <li class="nav-item">
+          <a class="nav-link"
+            href="{{url('/lista')}}">Lista</a>
 
+        </li>
+
+
+        {{-- </ul> --}}
+
+        @guest
+        <div class="collapse navbar-collapse"
+          id="navbarResponsive">
+          {{-- <ul class="navbar-nav ml-auto"> --}}
+          <li class="nav-item">
+            <a class="nav-link"
+              href="{{url('register')}}">Cadastrar-se</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link"
+              href="{{url('entrar')}}">Entrar</a>
+          </li>
       </ul>
-      
-      @guest
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-          <a class="nav-link" href="{{url('register')}}">Cadastrar-se</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link" href="{{url('entrar')}}">Entrar</a>
-          </li>
-        </ul>
-      </div>
-      @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->nome }} <span class="caret"></span>
-                                </a>
+    </div>
+    @else
+    {{-- <ul class="navbar-nav ml-auto"> --}}
+    <li class="nav-item">
+      <a class="nav-link"
+        href="{{url('perfil')}}">Perfil</a>
+    </li>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+    <li class="nav-item dropdown">
+
+      <a id="navbarDropdown"
+        class="nav-link dropdown-toggle" href="#"
+        role="button" data-toggle="dropdown"
+        aria-haspopup="true" aria-expanded="false" v-pre>
+        {{ Auth::user()->nome }} <span class="caret"></span>
+      </a>
+
+      <div class="dropdown-menu dropdown-menu-right"
+        aria-labelledby="navbarDropdown">
+        <a class="dropdown-item"
+          href="{{ route('logout') }}"
+          onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+          {{ __('Logout') }}
+        </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-      @endguest
+        <form id="logout-form"
+          action="{{ route('logout') }}" method="POST"
+          style="display: none;">
+          @csrf
+        </form>
+      </div>
+    </li>
+    </ul>
+    @endguest
+
+
     </div>
   </nav>
 
 
   {{-- CONTEUDO --}}
-  <div class="container" style="margin-top: 100px; margin-bottom: 100px; align: center;" >
+  <div class="container"
+    style="margin-top: 100px; margin-bottom: 100px; align: center;">
     @yield('conteudo')
   </div>
 
   <!-- Footer -->
   <footer class="py-3 bg-black fixed-bottom">
     <fieldset id="cores" style="margin-top: -10px">
-        <button type="button" id="botao" onclick="changeBG('#B39DDB')" class="btn btn-primary"></button>
-        <button type="button" id="botao" onclick="changeBG('#E6EE9C')" class="btn btn-success"></button>
-        <button type="button" id="botao" onclick="changeBG('#ef9a9a')" class="btn btn-danger"></button>
-        <button type="button" id="botao" onclick="changeBG('#FFF59D')" class="btn btn-warning"></button>
-        <button type="button" id="botao" onclick="changeBG('#eeeeee')" class="btn btn-secondary"></button>
+      <button type="button" id="botao"
+        onclick="changeBG('#B39DDB')"
+        class="btn btn-primary"></button>
+      <button type="button" id="botao"
+        onclick="changeBG('#E6EE9C')"
+        class="btn btn-success"></button>
+      <button type="button" id="botao"
+        onclick="changeBG('#ef9a9a')"
+        class="btn btn-danger"></button>
+      <button type="button" id="botao"
+        onclick="changeBG('#FFF59D')"
+        class="btn btn-warning"></button>
+      <button type="button" id="botao"
+        onclick="changeBG('#eeeeee')"
+        class="btn btn-secondary"></button>
     </fieldset>
     <div class="container">
-      <p class="m-0 text-center text-white small">Copyright &copy; ProjetoPATA 2019</p>
-      
+      <p class="m-0 text-center text-white small">Copyright
+        &copy; ProjetoPATA 2019</p>
+
     </div>
     <!-- /.container -->
   </footer>
 
   <!-- Bootstrap core JavaScript -->
-  <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script
+    src="/vendor/bootstrap/js/bootstrap.bundle.min.js">
+  </script>
 
 </body>
 
